c63e0c28834607491c5ff4d2a11c96226fb41686
Achmad Furqon Rachmadie <achmadfurqonrachmadie@gmail.com>
2023-11-12 12:03:10 +0700

    halfway 2nd module

---
+++ pertemuan2/JsFunctions.js
@@ -0,0 +1,88 @@
+/** =============================== 1st subject =====================
+ *  JAVASCRIPT FUNCTION
+ * this is a declaration method used before ES6 syntax
+ */
+
+// =====> declaration
+function power(base, exp) {
+  let total = 1;
+
+  for (let i = 0; i < exp; i++) {
+    total *= base;
+  }
+
+  return total;
+}
+
+/* Different ways of function invocation */
+
+// ====> normal invocation
+console.log(power(1, 3));
+
+// =====> invoke without parentheses ()
+console.log(power);
+
+// =====> Function as variable values
+let result = power(10, 4); // power will return 10^4 that is 10000
+console.log(result);
+
+
+/* ARROW FUNCTION
+ * New way to declare a function after ES6 
+ *
+ */
+
+// ====> Before arrow
+function sayHello() { 
+    // do something ...
+    return "Hello world";
+}
+
+// invocation
+// sayHello();
+
+
+// ====> arrow function
+let sayHey = () => {
+    // do something
+    return "Hello world";
+}
+
+// invocation
+// sayHey();
+
+
+/** =============================== 2nd subject =====================
+ * Function as an object's attribute
+ */
+
+// First, create an object of student
+const student = {
+    "name" : "Fahry", 
+    "age" : 25, 
+    "GPA" : 3.80,
+    "hobbies": ["UI/UX Design", "Hacking your laptop", "Sleep"],
+    "isActive": true,
+    "doWalking": function () {
+        return this.name + " is walking...";
+    },
+    "eatSomething": () => {
+        
+        return student.name + " is ate something strange...";
+    }
+}
+
+// Second, invoke each functions we had
+console.log(student.doWalking());
+console.log(student.eatSomething());
+
+
+
+/** =============================== 3rd subject =====================
+ * 1. Scope 
+ *  1.1. Local Scope 
+ *  1.2. Function Scope
+ *  1.3. Global Scope
+ * 2. Hoisting
+ * 
+ */
